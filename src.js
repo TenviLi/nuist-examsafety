cad314c4c876086017edd39ce9112b960e38e8c0
kingcc <laikinfox@gmail.com>
2018-12-13 17:34:26 +0800

    :sparkles: refactor: src.js

--- src.js
+++ src.js
@@ -1,21 +1,31 @@
-var a_tm = 0;
-setInterval(function() {
-    if (a_tm % 60 == 0) {
-        $.post("/exam_xuexi_online.php", { cmd: "xuexi_online" }, function(n) {
-            n = JSON.parse(n);
-            $(".block-login .explanation li:first").html(
-                "<a class='changePassword loginCommonBtn'>最近一次发送请求:" +
-                    n.shichang +
-                    "</a>"
-            );
-            if (-1 != n.shichang.indexOf("时")) alert("挂完了");
-        });
-    } else {
-        $(".block-login .explanation li:eq(1)").html(
-            "<a class='changePassword loginCommonBtn'>挂题库时长:" +
-                a_tm +
-                "</a>"
-        );
-        a_tm++;
+const later = (delay) => new Promise((resolve) => setTimeout(resolve, delay));
+const seconds = Array(60).fill(void 0).map((...[,idx]) => idx);
+
+/**
+ * @wrapper
+ * @asynchronous
+ */
+(async () => {
+  let minute = 0;
+  while (minute > -1) { // forever
+    $.post('/exam_xuexi_online.php', {cmd: 'xuexi_online'}, (data) => {
+      let shichang;
+      try {
+        ({shichang} = JSON.parse(data));
+      } catch (e) {
+        return new Error('parse error occurs');
+      }
+      $(".block-login .explanation li:first").html(`<a class='changePassword loginCommonBtn'>最近一次发送请求:${shichang}</a>`);
+      if (shichang.indexOf('时') > 0) {
+        return alert('挂完了');
+      }
+    });
+
+    for (const second of seconds) {
+      await later(1000);
+      $('.block-login .explanation li:eq(1)').html(`<a class='changePassword loginCommonBtn'>挂题库时长:${minute * 60 + second + 1}</a>`);
     }
-}, 1000);
+
+    minute++;
+  }
+})();
